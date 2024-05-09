1a3b05ac9aa118521c80190ffb5cd9c41a2b7446
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-05-09 22:29:00 +0700

    up logo

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -1,8 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #e3f2fd;">
     <a class="navbar-brand" href="{{ route('home') }}">
-        <img src="https://getbootstrap.com/docs/4.6/assets/brand/bootstrap-solid.svg" width="30" height="30"
-            class="d-inline-block align-top" alt="">
-        <b>SAVE</b>
+        <img src="{{ asset('images/asa.png') }}" height="30" class="d-inline-block align-top" alt="">
+        {{-- <b>SAVE</b> --}}
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
         aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
