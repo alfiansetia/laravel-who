162385bda32d15f9acbf82e7a21a429e46403c8a
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-06-25 17:09:38 +0700

    remove

--- config/app.php
+++ config/app.php
@@ -174,11 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
         Maatwebsite\Excel\ExcelServiceProvider::class,
-        Spatie\Permission\PermissionServiceProvider::class,
-
-        Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
 
 
 
