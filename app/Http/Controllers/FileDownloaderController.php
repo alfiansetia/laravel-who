5599d348bf6c26d8d59077f774faca0a7afb347f
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-07-24 20:08:33 +0700

    Update FileDownloaderController.php

--- app/Http/Controllers/FileDownloaderController.php
+++ app/Http/Controllers/FileDownloaderController.php
@@ -4,17 +4,22 @@ namespace App\Http\Controllers;
 
 use App\Models\Setting;
 use App\Traits\ResponseTrait;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class FileDownloaderController extends Controller
 {
     use ResponseTrait;
+    private $headers_file = [];
 
     public function __construct()
     {
         $setting = Setting::first();
         $this->headers['Cookie'] = 'session_id=' . $setting->odoo_session ?? '';
+        $this->headers_file['Cookie'] = 'session_id=' . $setting->odoo_session ?? '';
     }
 
     public function download(string $so = null)
@@ -54,8 +59,17 @@ class FileDownloaderController extends Controller
             $url = $odoo_domain . '/web/dataset/call_button';
             $response = Http::withHeaders($this->headers)->post($url, $data);
             $json = $response->json();
-            $data_url = $json['result']['url'];
-            return redirect($odoo_domain . $data_url);
+            $data_url = $odoo_domain . $json['result']['url'];
+            $fileResponse = Http::withHeaders($this->headers_file)->get($data_url);
+            $full = $fileResponse->header('Content-Disposition');
+            $rep = str_replace("attachment; filename*=UTF-8''", '', $full);
+            $jadi = urldecode($rep);
+            $filePath = public_path('files/' . $jadi);
+            if (!file_exists(public_path('files/'))) {
+                File::makeDirectory(public_path('files/'), 755, true);
+            }
+            file_put_contents($filePath, $fileResponse->body());
+            return response()->download($filePath)->deleteFileAfterSend();
         } catch (\Throwable $th) {
             abort(404, $th->getMessage());
         }
