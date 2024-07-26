e6f2c99f2cbe711374cbceb02205bfaf38b4c2c7
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-07-26 23:33:16 +0700

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,6 @@ use App\Http\Controllers\AlamatController;
 use App\Http\Controllers\BastController;
 use App\Http\Controllers\FileDownloaderController;
 use App\Http\Controllers\HomeController;
-use App\Http\Controllers\KontakController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\StockController;
 use Illuminate\Support\Facades\Route;
@@ -18,19 +17,7 @@ Route::get('products', [ProductController::class, 'index'])->name('products.inde
 
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-Route::get('products/download-sample', function () {
-    return response()->download(public_path('source/product.xls'));
-})->name('product.download.sample');
-
-
 Route::resource('alamat', AlamatController::class);
-
-Route::delete('/kontak', [KontakController::class, 'destroy'])->name('kontak.destroy');
-Route::resource('kontak', KontakController::class)->except('create', 'destroy');
-Route::get('kontak/download-sample', function () {
-    return response()->download(public_path('source/kontak.xls'));
-})->name('kontak.download.sample');
-
 
 Route::get('stock', [StockController::class, 'index'])->name('stock.index');
 Route::resource('bast', BastController::class);
