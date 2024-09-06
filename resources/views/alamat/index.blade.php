57c715d93f628e83039ebca74c0bd23c525c8f28
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2024-09-06 14:23:59 +0700

    Update index.blade.php

--- resources/views/alamat/index.blade.php
+++ resources/views/alamat/index.blade.php
@@ -41,9 +41,9 @@
                 ],
                 pageLength: 10,
                 lengthChange: false,
-                // order: [
-                //     [0, "asc"]
-                // ],
+                order: [
+                    [0, "desc"]
+                ],
                 columns: [{
                         data: 'id',
                         render: function(data, type, row, meta) {
