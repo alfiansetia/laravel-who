0cdd12c430752e5c7150a6bc71630de0b82fed63
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-06-03 08:49:09 +0700

    Update sn.blade.php

--- resources/views/tools/sn.blade.php
+++ resources/views/tools/sn.blade.php
@@ -50,14 +50,9 @@
                         <div class="form-group col-12">
                             <label for="skip">Skip</label>
                             <select name="skip" id="skip" class="form-control" multiple>
-                                <option value="1">1</option>
-                                <option value="2">2</option>
-                                <option value="3">3</option>
-                                <option value="4">4</option>
-                                <option value="5">5</option>
-                                <option value="6">6</option>
-                                <option value="7">7</option>
-                                <option value="8">8</option>
+                                @for ($i = 0; $i < 20; $i++)
+                                    <option value="{{ $i }}">{{ $i }}</option>
+                                @endfor
                             </select>
                         </div>
                         <br>MDS skip 2 & 4
