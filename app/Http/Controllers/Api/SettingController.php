0acb6727cea2fe4a4b8d620e08897907aee4806f
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-10-30 14:46:24 +0700

    Update SettingController.php

--- app/Http/Controllers/Api/SettingController.php
+++ app/Http/Controllers/Api/SettingController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Artisan;
 
 class SettingController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('env_auth')->only(['test_notif', 'store', 'reload']);
+    }
+
     public function index()
     {
         $session = OdooSession::getCurrentSession();
