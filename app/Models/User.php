5989dd993f2bf995e8ab6596390634f2e3d93cdc
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2025-12-03 13:17:03 +0700

    remove filament!

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,14 +4,12 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 
-use Filament\Models\Contracts\FilamentUser;
-use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable implements FilamentUser
+class User extends Authenticatable
 
 {
     use HasFactory, Notifiable, HasApiTokens;
@@ -48,10 +46,5 @@ class User extends Authenticatable implements FilamentUser
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
-    }
-
-    public function canAccessPanel(Panel $panel): bool
-    {
-        return str_ends_with($this->email, '@gmail.com');
     }
 }
