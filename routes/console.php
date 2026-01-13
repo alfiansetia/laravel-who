1c28777af80f1d02eb1e64eede1ffcc7a4174adc
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2026-01-13 13:51:36 +0700

    Update console.php

--- routes/console.php
+++ routes/console.php
@@ -10,15 +10,23 @@ use Illuminate\Support\Facades\Schedule;
 // })->purpose('Display an inspiring quote')->hourly();
 
 
-Schedule::command('app:monitor-do')->everyMinute()->onSuccess(function () {
-    // Log::info('Cronjob Monitor DO berhasil dijalankan');
-})->onFailure(function () {
-    // Log::error('Cronjob Monitor DO Gagal dijalankan');
-});
+Schedule::command('app:monitor-do')
+    ->everyMinute()
+    ->weekdays()
+    ->between('06:00', '18:00')
+    ->onSuccess(function () {
+        // Log::info('Cronjob Monitor DO berhasil dijalankan');
+    })->onFailure(function () {
+        // Log::error('Cronjob Monitor DO Gagal dijalankan');
+    });
 
 
-Schedule::command('app:odoo-login')->hourly()->onSuccess(function () {
-    // Log::info('Cronjob Monitor DO berhasil dijalankan');
-})->onFailure(function () {
-    // Log::error('Cronjob Monitor DO Gagal dijalankan');
-});
+Schedule::command('app:odoo-login')
+    ->hourly()
+    ->weekdays()
+    ->between('05:00', '18:00')
+    ->onSuccess(function () {
+        // Log::info('Cronjob Monitor DO berhasil dijalankan');
+    })->onFailure(function () {
+        // Log::error('Cronjob Monitor DO Gagal dijalankan');
+    });
