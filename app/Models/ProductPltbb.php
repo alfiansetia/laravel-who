e752398e0ed275120056a6574f6634925e22bf9a
Alfian Setiawan <56498155+alfiansetia@users.noreply.github.com>
2026-04-05 12:09:39 +0700

    Update ProductPltbb.php

--- app/Models/ProductPltbb.php
+++ app/Models/ProductPltbb.php
@@ -11,6 +11,15 @@ class ProductPltbb extends Model
 
     protected $appends = ['is_complete'];
 
+    protected $casts = [
+        'id' => 'integer',
+        'product_id' => 'integer',
+        'p' => 'decimal:2',
+        'l' => 'decimal:2',
+        't' => 'decimal:2',
+        'b' => 'decimal:2',
+    ];
+
     public function getIsCompleteAttribute()
     {
         return $this->p > 0 && $this->l > 0 && $this->t > 0 && $this->b > 0;
